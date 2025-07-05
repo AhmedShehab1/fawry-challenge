db614d99b54849a46281f5049b3e69a6ee85e23e
AhmedShehab1 <legendbtw2003@gmail.com>
2025-07-05 15:44:25 +0300

    Add ConsoleShippingService implementation and define ShippingService interface for shipping functionality

---
+++ src/com/ecommerce/shipping/ConsoleShippingService.java
@@ -0,0 +1,28 @@
+package com.ecommerce.shipping;
+
+import java.util.Map;
+
+import com.ecommerce.model.product.Shippable;
+
+public class ConsoleShippingService implements ShippingService {
+
+    @Override
+    public void shipItems(Map<Shippable, Integer> items) {
+
+        System.out.println("** Shipment notice **");
+
+        double totalWeight = 0;
+            for (Map.Entry<Shippable, Integer> entry : items.entrySet()) {
+                String name = entry.getKey().getName();
+                int quantity = entry.getValue();
+                double weight = entry.getKey().getWeight();
+                totalWeight += weight * quantity;
+                
+                System.out.printf("%dx %-20s %6.0fg%n", 
+                quantity, name, weight * 1000);
+            }
+            
+            System.out.printf("Total package weight %.1fkg%n%n", totalWeight);
+    }
+   
+}
--- src/com/ecommerce/shipping/ShippingService.java
+++ src/com/ecommerce/shipping/ShippingService.java
@@ -1,1 +1,9 @@
 package com.ecommerce.shipping;
+import java.util.Map;
+
+import com.ecommerce.model.product.Shippable;
+
+
+public interface ShippingService {
+    void shipItems(Map<Shippable, Integer> items);
+}
